f7b45403a8f6042cac6ded2b1e9e4d24c688bab4
Paul Maynard <Paul.Maynard001@umb.edu>
2020-01-28 17:03:03 -0500

    LUB prperty

--- Order.v
+++ Order.v
@@ -1,21 +1,32 @@
 Require Import Ensembles.
 
 
-Class TotalOrder (S : Set) (LT : S -> S -> Prop) :=
+Class Totalorder (S : Set) (LT : S -> S -> Prop) :=
 { antisym : forall a b, LT a b -> LT b a -> a = b
 ; trans : forall a b c, LT a b -> LT b c -> LT a c
 ; connex : forall a b, LT a b /\ LT b a
 }.
 
-Definition IsUpperBound {A} {LT} {_ : TotalOrder A LT} (E : Ensemble A) (b: A)
+Definition UpperBound {A} {LT} {ord : Totalorder A LT} (E : Ensemble A) (b: A) : Prop
   := forall a, In _ E a -> LT a b.
 
-Definition IsLeastUpperBound {A} {LT} {_ : TotalOrder A LT} (E : Ensemble A) (b: A)
-  := IsUpperBound E b /\ (forall c, b = c /\ IsUpperBound E c -> LT b c).
+Definition LeastUpperBound {A} {LT} {ord : Totalorder A LT} (E : Ensemble A) (b: A) : Prop
+  := UpperBound E b /\ (forall c, b = c /\ UpperBound E c -> LT b c).
 
+Definition UpperBounded {A} {LT} {ord : Totalorder A LT} (E : Ensemble A): Prop
+  := exists b, UpperBound E b.
 
-Definition IsLowerBound {A} {LT} {_ : TotalOrder A LT} (E : Ensemble A) (b: A)
+Definition UpperBoundProperty A LT {ord : Totalorder A LT} : Prop
+  := forall E : Ensemble A, UpperBounded E -> exists b, LeastUpperBound E b.
+
+Definition LowerBound {A} {LT} {ord : Totalorder A LT} (E : Ensemble A) (b: A) : Prop
   := forall a, In _ E a -> LT b a.
 
-Definition IsGreatestLowerBound {A} {LT} {_ : TotalOrder A LT} (E : Ensemble A) (b: A)
-  := IsLowerBound E b /\ (forall c, b = c /\ IsLowerBound E c -> LT c b).
+Definition GreatestLowerBound {A} {LT} {ord : Totalorder A LT} (E : Ensemble A) (b: A)
+  := LowerBound E b /\ (forall c, b = c /\ LowerBound E c -> LT c b).
+
+Definition LowerBounded {A} {LT} {ord : Totalorder A LT} (E : Ensemble A): Prop
+  := exists b, LowerBound E b.
+
+Definition LowerBoundProperty A LT {ord : Totalorder A LT} : Prop
+  := forall E : Ensemble A, LowerBounded E -> exists b, GreatestLowerBound E b.
